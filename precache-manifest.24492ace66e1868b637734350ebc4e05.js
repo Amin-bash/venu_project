2e012fbbc6e3edc8a3441ed1b77bbb98b0f154d3
amin-bash <power.ameen99@gmail.com>
2020-07-06 11:15:36 +0300

    Updates

---
+++ precache-manifest.24492ace66e1868b637734350ebc4e05.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f5d4e72ba61592eef5247d54ad33773",
+    "url": "/venu_project/index.html"
+  },
+  {
+    "revision": "68ca4e7e73bef47f696e",
+    "url": "/venu_project/static/css/main.8027aaf8.chunk.css"
+  },
+  {
+    "revision": "220a8f526230c0b2c312",
+    "url": "/venu_project/static/js/2.bdd6daf8.chunk.js"
+  },
+  {
+    "revision": "0d93e3e8a3ba404e9974c77aeb071cf3",
+    "url": "/venu_project/static/js/2.bdd6daf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68ca4e7e73bef47f696e",
+    "url": "/venu_project/static/js/main.19b47fc1.chunk.js"
+  },
+  {
+    "revision": "6f306eaeb3be74edb403",
+    "url": "/venu_project/static/js/runtime-main.08a0f939.js"
+  },
+  {
+    "revision": "84753208e3ace6c36a3a29c731983a69",
+    "url": "/venu_project/static/media/slide_one.84753208.jpg"
+  },
+  {
+    "revision": "9aa09a50b7eb84e96d522c10dfaa78e5",
+    "url": "/venu_project/static/media/slide_three.9aa09a50.jpg"
+  },
+  {
+    "revision": "3a3ffd3696fea8999087bbdcf97d69d2",
+    "url": "/venu_project/static/media/slide_two.3a3ffd36.jpg"
+  }
+]);
